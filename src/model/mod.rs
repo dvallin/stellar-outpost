982ec345b125a2afd0f8402cc91a8c2f340b89c9
Maximilian Stribeck <schulermaximilian@gmail.com>
2023-06-19 12:01:51 +0200

    add regions

--- src/model/mod.rs
+++ src/model/mod.rs
@@ -1,10 +1,11 @@
-use self::{game_state::GameState, outpost::Outpost};
+use self::{game_state::GameState, outpost::Outpost, region::Region};
 use serde::{Deserialize, Serialize};
 
 pub mod crew;
 pub mod game_state;
 pub mod modules;
 pub mod outpost;
+pub mod region;
 pub mod resources;
 pub mod stats;
 
@@ -12,6 +13,7 @@ pub mod stats;
 pub struct Game {
     pub state: GameState,
     pub outpost: Outpost,
+    pub region: Region,
 }
 
 impl Game {
@@ -19,11 +21,13 @@ impl Game {
         Self {
             state: GameState::new(),
             outpost: Outpost::new(),
+            region: Region::new(),
         }
     }
 
     pub fn finish_turn(&mut self) {
         self.outpost.finish_turn(&mut self.state);
         self.state.finish_turn();
+        self.region.finish_turn();
     }
 }
